475a4d7781e06658c7b44fc62279c61e7a368107
aaronshivers <aaron@moonmail.org>
2020-04-05 09:54:19 -0500

    start styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+import { AboutComponent } from './about/about.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    AboutComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
